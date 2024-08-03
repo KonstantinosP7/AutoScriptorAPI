c479ceac0742db49b3de1c27853eb29295fabc47
Konstantinos Polychronopoulos <konstantinos.polychronopoulos@cuberm.com>
2024-08-03 11:30:48 +0300

    Add swagger to release package

--- AutoScriptorAPI/Program.cs
+++ AutoScriptorAPI/Program.cs
@@ -17,11 +17,8 @@ builder.Services.Configure<CommunicationSettings>(builder.Configuration.GetSecti
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
